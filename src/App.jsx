b8e1a9da569b3f3d7d855f6e655022d43825e438
Muhamad Renald <avryso@gmail.com>
2025-02-28 07:38:29 +0700

    feat: update file App.jsx and assets.js

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 // import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import About from "./components/About";
+import Projects from "./components/Projects";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       {/* <Navbar /> */}
       <Header />
       <About />
+      <Projects />
     </div>
   );
 };
